aee65e2ed0f2e3961eec6803f5b73d0504d20641
Kantann <quentin.cmpn@gmail.com>
2021-04-05 00:06:01 +0200

    ajout slider projets

---
+++ assets/scripts/slider.js
@@ -0,0 +1,70 @@
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
+
+var currentSlide = 0; //first displayed slide is always 0 by default
+var slider = document.getElementById("slider");
+var sliderChilds = document.querySelectorAll(".slider_child");
+var slidesAvailable = sliderChilds.length;
+const slideDelay = 500; // in millis
+
+//slide to offset slide from the current
+function slide(offset){
+    if(offset == 0)return;
+    if(offset > 0){
+        slideRight();
+        if(offset-1 != 0){//if not last element
+            sleep(slideDelay).then(() => {
+                slide(offset-1);
+            });
+        }
+    }else{
+        slideLeft();
+        if(offset+1 != 0){//if not last element
+            sleep(slideDelay).then(() => {
+                slide(offset+1);
+            });
+        }
+    }
+}
+
+//swipe display of given elems
+function swipe(oldElem, newElem){
+    hideElem(oldElem);
+    showElem(newElem);
+}
+
+function slideRight(){
+    var targetSlide = mod(currentSlide+1, slidesAvailable);
+    swipe(sliderChilds[currentSlide], sliderChilds[targetSlide]);
+    currentSlide = targetSlide;
+}
+
+function slideLeft(){
+    var targetSlide = mod(currentSlide-1, slidesAvailable);
+    swipe(sliderChilds[currentSlide], sliderChilds[targetSlide]);
+    currentSlide = targetSlide;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function hideElem(elem){
+    elem.style.opacity = "0";
+}
+
+function showElem(elem){
+    elem.style.opacity = "1";
+}
+
+// --------------------------  MAIN  ---------------------------------------
+
+//hide all slide excepts selected one
+for(var i = 0 ; i < slidesAvailable ; i++){
+    if(i != currentSlide){
+        hideElem(sliderChilds[i]);
+    }else{
+        showElem(sliderChilds[i]);
+    }
+}
